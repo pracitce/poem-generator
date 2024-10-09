33349b59d17c5f9001cbf2c6295c6d791ffd50ea
Learn132 <109186405+pracitce@users.noreply.github.com>
2024-10-08 21:21:44 -0400

    basic html, js, add id , form typerwriter plug in

---
+++ src/index.js
@@ -0,0 +1,18 @@
+function generatePoem(event) {
+    event.preventDefault(); 
+
+
+
+    new Typewriter("#poem", {
+        strings: "La tombe dit à la rose",
+        autoStart: true,
+        delay: 1,
+        cursor: "",
+      });
+
+
+   
+}
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
